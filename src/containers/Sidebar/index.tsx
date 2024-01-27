c5cf3d6fc9d78249b1a247df59855a9b64950c17
Elisafa <elisafajunior@hotmail.com>
2024-01-27 15:58:53 -0300

    Adicionando o tema dark

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -3,7 +3,11 @@ import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
 import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
-const Sidebar = () => {
+type Props = {
+  trocaTema: () => void
+}
+
+const Sidebar = (props: Props) => {
   return (
     <>
       <aside>
@@ -18,7 +22,7 @@ const Sidebar = () => {
           <Descricao tipo="principal" textAlingn={'center'}>
             Engenheiro FullStack
           </Descricao>
-          <BotaoTema>Trocar tema</BotaoTema>
+          <BotaoTema onClick={props.trocaTema}>Trocar tema</BotaoTema>
         </SidebarContainer>
       </aside>
     </>
